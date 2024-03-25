44678e23415e26ea8cc9d96f687f96cd90b8bff6
Issah45 <90643234+Issah45@users.noreply.github.com>
2024-03-26 00:15:37 +0800

    Add files via upload

---
+++ backup.py
@@ -0,0 +1,18 @@
+platforms = [
+	pygame.Rect(160, 384, 704, 64),
+	pygame.Rect(160, 192, 128, 192),
+	pygame.Rect(768, 192, 96, 192),
+	pygame.Rect(288, 192, 480, 160),
+]
+spikes = [
+	Spike(672, 352),
+	Spike(640, 352),
+	Spike(608, 352),
+	Spike(576, 352),
+	Spike(544, 352),
+	Spike(512, 352),
+]
+
+manik_x = 320
+manik_y = 352
+manik=True
