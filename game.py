3ed5cc3027e73eea583442fba45ef6f4f9e745fe
Issah45 <issuseless4@gmail.com>
2024-08-09 15:25:42 +0800

    Hmm... dash

--- game.py
+++ game.py
@@ -48,17 +48,17 @@ pygame.display.set_caption("LManiks")
 
 # Functions
 def approx(a):
-	a = round(a/tile_size) * tile_size
+        a = round(a/tile_size) * tile_size
 
 def restart():
-	# platforms = []
-	eol = EOL(eol_x, eol_y)
-	player.x = player_x
-	player.y = player_y
-	player.rect_update()
+        # platforms = []
+        eol = EOL(eol_x, eol_y)
+        player.x = player_x
+        player.y = player_y
+        player.rect_update()
 
 def round32(what):
-	return round(what/32) * 32
+        return round(what/32) * 32
 
 # Variables & O
 player_x, player_y = 0, 0
@@ -71,178 +71,193 @@ qua = pygame.Surface((50, 50))
 
 # Game Loop
 while True:
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-		if event.type == KEYDOWN:
-			if event.key == K_c and blocks < block_lim:
-				if player.left: x = round32(player.x) - 32
-				else: x = round32(player.x) + 32
-				y = round32(player.y)
+        for event in pygame.event.get():
+                if event.type == QUIT:
+                        pygame.quit()
+                if event.type == KEYDOWN:
+                        if event.key == K_c and blocks < block_lim:
+                                if player.left: x = round32(player.x) - 32
+                                else: x = round32(player.x) + 32
+                                y = round32(player.y)
 
-				r = pygame.Rect(x, y, 32, 32)
+                                r = pygame.Rect(x, y, 32, 32)
 
-				if player.rect.colliderect(r):
-					if player.left: x -= 32
-					else: x += 32
-					r = pygame.Rect(x, y, 32, 32)
-				blocks += 1
+                                if player.rect.colliderect(r):
+                                        if player.left: x -= 32
+                                        else: x += 32
+                                        r = pygame.Rect(x, y, 32, 32)
+                                blocks += 1
 
-				platforms.append(r)
-			if event.key == K_r:
-				exec(open(f"levels/level{level}.py").read())
-				restart()
-				player.platforms = platforms
-				screen_shake = 100
-				blocks = 0
-	display.fill((50, 150, 250))
-	# Background
-	if level <= 9:
-		display.blit(bg, (0, 0))
-	if level == 9:
-		display.blit(bg2, (round(width*2/3), 0))
-	# Platforms
-	for platform in platforms:
-		w = round(platform.width / tile_size)
-		h = round(platform.height / tile_size)
-		img = pygame.image.load("images/tile4.png").convert()
-		imger = pygame.transform.scale(img, (tile_size, tile_size))
-		for i in range(w):
-			display.blit(imger, (platform.x+(i*tile_size), platform.y))
-			for j in range(h):
-				display.blit(imger, (platform.x+(i*tile_size), platform.y+(j*tile_size)))
+                                platforms.append(r)
+                        if event.key == K_r:
+                                exec(open(f"levels/level{level}.py").read())
+                                restart()
+                                player.platforms = platforms
+                                screen_shake = 100
+                                blocks = 0
+        display.fill((50, 150, 250))
+        # Background
+        if level <= 9:
+                display.blit(bg, (0, 0))
+        if level == 9:
+                display.blit(bg2, (round(width*2/3), 0))
+        # Platforms
+        for platform in platforms:
+                w = round(platform.width / tile_size)
+                h = round(platform.height / tile_size)
+                img = pygame.image.load("images/tile4.png").convert()
+                imger = pygame.transform.scale(img, (tile_size, tile_size))
+                for i in range(w):
+                        display.blit(imger, (platform.x+(i*tile_size), platform.y))
+                        for j in range(h):
+                                display.blit(imger, (platform.x+(i*tile_size), platform.y+(j*tile_size)))
 
-	# Updates
-	player.update()
+        # Updates
+        player.update()
 
-	# Renders
-	player.render()
-	eol.render()
+        # Renders
+        player.render()
+        eol.render()
 
-	# Bounds
-	if player.y > 650:
-		exec(open(f"levels/level{level}.py").read())
-		restart()
-		player.platforms = platforms
-		screen_shake = 100
-		blocks = 0
+        # Bounds
+        if player.y > 650:
+                exec(open(f"levels/level{level}.py").read())
+                restart()
+                player.platforms = platforms
+                screen_shake = 100
+                blocks = 0
 
-	# Collisions & Stuff
-	for spike in spikes:
-		spike.render()
-		if spike.rect.colliderect(player.rect):
-			exec(open(f"levels/level{level}.py").read())
-			restart()
-			player.platforms = platforms
-			screen_shake = 100
-			blocks = 0
-	
-	if player.rect.colliderect(eol.rect):
-		level += 1
-		exec(open(f"levels/level{level}.py").read())
-		eol = EOL(eol_x, eol_y)
-		player = Player(player_x, player_y, platforms)
-		transition = 255
-		blocks = 0
-		open("levelsave", "w").write(str(level))
-	
-	for lavaball in lavaballs:
-		lavaball.render()
-		lavaball.update()
-		if player.rect and player.rect.colliderect(lavaball.rect):
-			exec(open(f"levels/level{level}.py").read())
-			restart()
-			player.platforms = platforms
-			screen_shake = 100
-			blocks = 0
+        # Collisions & Stuff
+        for spike in spikes:
+                spike.render()
+                if spike.rect.colliderect(player.rect):
+                        exec(open(f"levels/level{level}.py").read())
+                        restart()
+                        player.platforms = platforms
+                        screen_shake = 100
+                        blocks = 0
+        
+        if player.rect.colliderect(eol.rect):
+                level += 1
+                exec(open(f"levels/level{level}.py").read())
+                eol = EOL(eol_x, eol_y)
+                player = Player(player_x, player_y, platforms)
+                transition = 255
+                blocks = 0
+                open("levelsave", "w").write(str(level))
+        
+        for lavaball in lavaballs:
+                lavaball.render()
+                lavaball.update()
+                if player.rect and player.rect.colliderect(lavaball.rect):
+                        exec(open(f"levels/level{level}.py").read())
+                        restart()
+                        player.platforms = platforms
+                        screen_shake = 100
+                        blocks = 0
 
-	# Dialog
-	for dialog in dialogs:
-		dialog.render()
-		dialog.update()
-		try: dialog.rect.colliderect(player.rect)
-		except: pass
-		else:
-			if dialog.rect.colliderect(player.rect):
-				dialog.tooltip = True
-			else:
-				dialog.tooltip = False
-			
-			if dialog.rect.colliderect(player.rect) and pygame.key.get_pressed()[K_x]:
-				dialog.dia = True
-			
-			if dialog.dia and pygame.key.get_pressed()[K_DOWN] and not holdinger:
-				dialog.index += 1
-			if dialog.dia and pygame.key.get_pressed()[K_UP] and not holdinger:
-				dialog.index -= 1
-			if dialog.index >= len(dialog.content):
-				dialog.dia = False
-				dialog.index = 0
-			if dialog.index < 0:
-				dialog.index = 0
-			
-			if pygame.key.get_pressed()[K_UP] or pygame.key.get_pressed()[K_DOWN]:
-				holdinger = True
-			else:
-				holdinger = False
+        # Dialog
+        for dialog in dialogs:
+                dialog.render()
+                dialog.update()
+                try: dialog.rect.colliderect(player.rect)
+                except: pass
+                else:
+                        if dialog.rect.colliderect(player.rect):
+                                dialog.tooltip = True
+                        else:
+                                dialog.tooltip = False
+                        
+                        if dialog.rect.colliderect(player.rect) and pygame.key.get_pressed()[K_x]:
+                                dialog.dia = True
+                        
+                        if dialog.dia and pygame.key.get_pressed()[K_DOWN] and not holdinger:
+                                dialog.index += 1
+                        if dialog.dia and pygame.key.get_pressed()[K_UP] and not holdinger:
+                                dialog.index -= 1
+                        if dialog.index >= len(dialog.content):
+                                dialog.dia = False
+                                dialog.index = 0
+                        if dialog.index < 0:
+                                dialog.index = 0
+                        
+                        if pygame.key.get_pressed()[K_UP] or pygame.key.get_pressed()[K_DOWN]:
+                                holdinger = True
+                        else:
+                                holdinger = False
 
-	# Tanker
-	if level == 8 and tanker.hp > 0:
-		bossmode = 1
-	if bossmode == 1:
-		if tanker.hp < 1:
-			bossmode = 0
-			eol.x = 832
-			eol.y = 512
-			eol.rect_update()
-		
-		if tanker_delay < 1 and tanker.f < -100:
-			tanker_bullets.append(TankerBullet(tanker.x+200, tanker.y+100, rand(0, 180)))
-			tanker_thugs.append(TankerThug(tanker.x+200, tanker.y+100, rand(0, 180)))
-			tanker_delay = 4
-		
-		for tanker_bullet in tanker_bullets:
-			tanker_bullet.render()
-			tanker_bullet.update()
-			if player.rect.colliderect(tanker_bullet.rect):
-				exec(open(f"levels/level{level}.py").read())
-				restart()
-				player.platforms = platforms
-				screen_shake = 100
-				tanker.hp = tanker.full_hp
-				blocks = 0
-		
-		for tanker_thug in tanker_thugs:
-			tanker_thug.render()
-			tanker_thug.update()
-			
-			if player.rect.colliderect(tanker_thug.rect) and player.dashing and not tanker_thug.o:
-				tanker_thug.dir = -tanker_thug.dir
-				tanker_thug.o = True
-				player.speed_x = -10
-			
-			if tanker.rect.colliderect(tanker_thug.rect) and tanker_thug.o:
-				tanker.e = -5
-				tanker.hp -= 8
-				tanker_thugs.remove(tanker_thug)
-		tanker.render()
-		tanker.update()
-		tanker_delay -= 1
+        # Tanker
+        if level == 8 and tanker.hp > 0:
+                bossmode = 1
+        if bossmode == 1:
+                if tanker.hp < 1:
+                        bossmode = 0
+                        eol.x = 832
+                        eol.y = 512
+                        eol.rect_update()
+                
+                if tanker_delay < 1 and tanker.f < -100:
+                        tanker_bullets.append(TankerBullet(tanker.x+200, tanker.y+100, rand(0, 180)))
+                        tanker_thugs.append(TankerThug(tanker.x+200, tanker.y+100, rand(0, 180)))
+                        tanker_delay = 4
+                
+                for tanker_bullet in tanker_bullets:
+                        tanker_bullet.render()
+                        tanker_bullet.update()
+                        if player.rect.colliderect(tanker_bullet.rect):
+                                exec(open(f"levels/level{level}.py").read())
+                                restart()
+                                player.platforms = platforms
+                                screen_shake = 100
+                                tanker.hp = tanker.full_hp
+                                blocks = 0
+                
+                for tanker_thug in tanker_thugs:
+                        tanker_thug.render()
+                        tanker_thug.update()
+                        
+                        if player.rect.colliderect(tanker_thug.rect) and player.dashing and not tanker_thug.o:
+                                tanker_thug.dir = -tanker_thug.dir
+                                tanker_thug.o = True
+                                player.speed_x = -10
+                        
+                        if tanker.rect.colliderect(tanker_thug.rect) and tanker_thug.o:
+                                tanker.e = -5
+                                tanker.hp -= 8
+                                tanker_thugs.remove(tanker_thug)
+                tanker.render()
+                tanker.update()
+                tanker_delay -= 1
 
-		tankw = tanker.img.get_width() - 5
-		pygame.draw.rect(display, (255, 0, 0), (tanker.x, tanker.y-20, tankw, 20))
-		pygame.draw.rect(display, (0, 255, 100), (tanker.x, tanker.y-20, tankw * (tanker.hp / tanker.full_hp), 20))
+                tankw = tanker.img.get_width() - 5
+                pygame.draw.rect(display, (255, 0, 0), (tanker.x, tanker.y-20, tankw, 20))
+                pygame.draw.rect(display, (0, 255, 100), (tanker.x, tanker.y-20, tankw * (tanker.hp / tanker.full_hp), 20))
 
-	trans.set_alpha(transition)
-	display.blit(trans, (0, 0))
-	transition -= 20
+        trans.set_alpha(transition)
+        display.blit(trans, (0, 0))
+        transition -= 20
 
-	if screen_shake > 0:
-		rand_value_x = rand(-10, 10) * (screen_shake / max_screen_shake)
-		rand_value_y = rand(-10, 10) * (screen_shake / max_screen_shake)
-		qua = pygame.transform.scale(display, (width, height)).convert()
-		display.blit(qua, (rand_value_x, rand_value_y))
-		screen_shake -= 1.25
-	
-	pygame.display.update()
-	clock.tick(60)
+        if screen_shake > 0:
+                rand_value_x = rand(-10, 10) * (screen_shake / max_screen_shake)
+                rand_value_y = rand(-10, 10) * (screen_shake / max_screen_shake)
+                qua = pygame.transform.scale(display, (width, height)).convert()
+                display.blit(qua, (rand_value_x, rand_value_y))
+                screen_shake -= 1.25
+
+        if player.left: x = round32(player.x) - 32
+        else: x = round32(player.x) + 32
+        y = round32(player.y)
+
+        r = pygame.Rect(x, y, 32, 32)
+
+        if player.rect:
+                if player.rect.colliderect(r):
+                        if player.left: x -= 32
+                        else: x += 32
+                        r = pygame.Rect(x, y, 32, 32)
+
+        if level >= 5 and not bossmode:
+                pygame.draw.rect(display, (30, 60, 125), r, 1)
+        
+        pygame.display.update()
+        clock.tick(60)
--- player.py
+++ player.py
@@ -3,102 +3,107 @@ import pygame
 pygame.init()
 
 class Player:
-	def __init__(self, x, y, platforms):
-		self.x = x
-		self.y = y
-		self.speed_y = 0
-		self.speed_x = 0
-		self.parent = pygame.display.get_surface()
-		self.idle = pygame.image.load("images/player/idle.png").convert_alpha()
-		self.jump = pygame.image.load("images/player/jump.png").convert_alpha()
-		self.fall = pygame.image.load("images/player/fall.png").convert_alpha()
-		self.dashing = False
-		self.current = self.idle
-		self.left = False
-		self.platforms = platforms
-		self.rect = None
-		self.jump_height = 8
-		self.hspeed_normal = 5
-		self.hspeed = 5
-		self.resistance = 0.75
-		self.falling = 0
-		self.gravity = 0.45
+        def __init__(self, x, y, platforms):
+                self.x = x
+                self.y = y
+                self.prevcord = [self.x, self.y]
+                self.speed_y = 0
+                self.speed_x = 0
+                self.parent = pygame.display.get_surface()
+                self.idle = pygame.image.load("images/player/idle.png").convert_alpha()
+                self.jump = pygame.image.load("images/player/jump.png").convert_alpha()
+                self.fall = pygame.image.load("images/player/fall.png").convert_alpha()
+                self.dashing = False
+                self.current = self.idle
+                self.left = False
+                self.platforms = platforms
+                self.rect = None
+                self.jump_height = 8
+                self.hspeed_normal = 5
+                self.hspeed = 5
+                self.resistance = 0.75
+                self.falling = 0
+                self.gravity = 0.45
 
-	def rect_update(self):
-		self.rect = pygame.Rect(self.x, self.y, self.current.get_width(), self.current.get_height())
+        def rect_update(self):
+                self.rect = pygame.Rect(self.x, self.y, self.idle.get_width(), self.idle.get_height())
 
-	def move_in_steps(self, steps):
-		self.falling += 1
-		for i in range(steps):
-			last_value = self.x
-			self.x += self.speed_x / steps
-			self.rect_update()
-			for p in self.platforms:
-				if p.colliderect(self.rect):
-					self.x = last_value
-					self.rect_update()
-					self.speed_x = 0
+        def move_in_steps(self, steps):
+                self.falling += 1
+                for i in range(steps):
+                        last_value = self.x
+                        self.x += self.speed_x / steps
+                        self.rect_update()
+                        for p in self.platforms:
+                                if p.colliderect(self.rect):
+                                        self.x = last_value
+                                        self.rect_update()
+                                        self.speed_x = 0
 
-			last_value = self.y
-			self.y += self.speed_y / steps
-			self.rect_update()
-			for p in self.platforms:
-				if p.colliderect(self.rect):
-					self.y = last_value
-					self.rect_update()
-					if self.speed_y > 0:
-						self.falling = 0
-					self.speed_y = 0
+                        last_value = self.y
+                        self.y += self.speed_y / steps
+                        self.rect_update()
+                        for p in self.platforms:
+                                if p.colliderect(self.rect):
+                                        self.y = last_value
+                                        self.rect_update()
+                                        if self.speed_y > 0:
+                                                self.falling = 0
+                                        self.speed_y = 0
 
-	def update(self):
-		self.rect_update()
+        def update(self):
+                self.rect_update()
 
-		# self.y += self.speed_y
+                # self.y += self.speed_y
 
-		# for p in self.platforms:
-		# 	if p.colliderect(self.rect):
-		# 		self.fix_overlap(p)
-		# 		self.speed_y = 0
+                # for p in self.platforms:
+                #       if p.colliderect(self.rect):
+                #               self.fix_overlap(p)
+                #               self.speed_y = 0
 
-		keys = pygame.key.get_pressed()
+                keys = pygame.key.get_pressed()
 
-		if keys[pygame.K_z] and self.falling < 5:
-			self.speed_y = -self.jump_height
+                if keys[pygame.K_z] and self.falling < 5:
+                        self.speed_y = -self.jump_height
 
-		if keys[pygame.K_RIGHT]:
-			self.speed_x = self.hspeed
-			self.left = False
-		if keys[pygame.K_LEFT]:
-			self.speed_x = -self.hspeed
-			self.left = True
-		
-		if keys[pygame.K_x]:
-			self.hspeed = 15
-			self.dashing = True
-		else:
-			self.hspeed = self.hspeed_normal
-			self.dashing = False
+                if keys[pygame.K_RIGHT]:
+                        self.speed_x = self.hspeed
+                        self.left = False
+                if keys[pygame.K_LEFT]:
+                        self.speed_x = -self.hspeed
+                        self.left = True
+                
+                if keys[pygame.K_x]:
+                        self.hspeed = 15
+                        self.dashing = True
+                else:
+                        self.hspeed = self.hspeed_normal
+                        self.dashing = False
 
-		self.speed_y += self.gravity
-		self.speed_x = self.speed_x * self.resistance
+                self.speed_y += self.gravity
+                self.speed_x = self.speed_x * self.resistance
 
-		if self.x < 0:
-			self.x = 0
-		if self.x > 928:
-			self.x = 928
+                if self.x < 0:
+                        self.x = 0
+                if self.x > 928:
+                        self.x = 928
 
-		self.move_in_steps(round(abs(self.speed_y) + abs(self.speed_x)))
-	
-	def render(self):
-		if self.left:
-			self.current = pygame.transform.flip(self.current, 1, 0)
-		
-		self.parent.blit(self.current, (self.x, self.y))
+                self.move_in_steps(round(abs(self.speed_y) + abs(self.speed_x)))
+                q = pygame.image.load("images/player/idle.png")
+                q.set_alpha(100)
+                self.parent.blit(q, self.prevcord)
+                self.prevcord = [self.x, self.y]
+        
+        def render(self):
+                if self.left:
+                        self.current = pygame.transform.flip(self.current, 1, 0)
+                
+                self.parent.blit(self.current, (self.x, self.y))
 
-		if self.falling < 3:
-			self.current = self.idle
-		else:
-			if self.speed_y > 0:
-				self.current = self.fall
-			else:
-				self.current = self.jump
+                if self.falling < 3:
+                        self.current = self.idle
+                else:
+                        if self.speed_y > 0:
+                                self.current = self.fall
+                        else:
+                                self.current = self.jump
